62cd1d38789f74e07a3ca9ca43e3fb4c57d43b1b
thomasbyoung <thomasbyoung12345@gmail.com>
2021-05-03 16:19:20 -0500

    basic express running

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public', 'index.html'));
-});
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 5000;
 
